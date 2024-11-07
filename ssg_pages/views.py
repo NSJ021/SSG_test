172f813791c1b0e27d761d3491456bcd62d9cfe0
Nathan Jones <nsjones021@gmail.com>
2024-11-07 13:37:12 +0000

    Nav links, styling

--- ssg_pages/views.py
+++ ssg_pages/views.py
@@ -5,3 +5,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'ssg_pages/home.html')
+
+def games(request):
+    return render(request, 'ssg_pages/games.html')
+
+def about(request):
+    return render(request, 'ssg_pages/about.html')
+
+def blog(request):
+    return render(request, 'ssg_pages/blog.html')
+
+def contact(request):
+    return render(request, 'ssg_pages/contact.html')
