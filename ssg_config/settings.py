3a85cb3081ca1e2bbee3245d9f52e296d8175cee
Nathan Jones <nsjones021@gmail.com>
2024-11-08 09:35:29 +0000

    Database deployment

--- ssg_config/settings.py
+++ ssg_config/settings.py
@@ -13,9 +13,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 
 import os
-
+import sys
 import dj_database_url
 
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -30,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-nsj021-ssgtest-x1nw5v5kv25.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -86,10 +88,18 @@ DATABASES = {
 if 'test' in sys.argv:
     DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
+
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
+
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'ssg_db',
+#         'USER': 'postgres',
+#         'PASSWORD': 'NJ_021!',
+#         'HOST': 'localhost',
 #     }
 # }
 
